15e59d94738074145456b4b211b425bc32274bb0
chris01 <chris_odum@yahoo.com>
2023-05-17 09:14:50 +0200

    Create Tag Class in models.py

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Tag(models.Model):
+    caption = models.CharField(max_length=20)
+    def __str__(self):
+        return f"#{self.caption} "
